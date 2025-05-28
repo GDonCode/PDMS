5aaad7a7cc322cee1f654dcc51bfc6251024618f
GDonCode <gavinrayne2@gmail.com>
2025-05-28 15:47:50 -0500

    set defualt launch to /login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,5 @@
-'use client'
+import { redirect } from 'next/navigation';
 
-import Link from 'next/link'
-
-export default function HomePage() {
-  return (
-    <main className="flex flex-col gap-2 items-center justify-center min-h-screen bg-gray-50">
-      <Link
-        href="/login"
-        className="px-6 py-3 text-white bg-blue-600 rounded hover:bg-blue-700 transition"
-      >
-        Login
-      </Link>
-      <Link
-        href="/register"
-        className="px-6 py-3 text-white bg-blue-600 rounded hover:bg-blue-700 transition"
-      >
-        Register
-      </Link>
-      <Link
-        href="/test"
-        className="px-6 py-3 text-white bg-blue-600 rounded hover:bg-blue-700 transition"
-      >
-        Test
-      </Link>
-      <Link
-        href="/admin"
-        className="px-6 py-3 text-white bg-blue-600 rounded hover:bg-blue-700 transition"
-      >
-        Admin
-      </Link>
-    </main>
-  )
+export default function Home() {
+  redirect('/login');
 }
